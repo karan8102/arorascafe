ec23d077f3b06c77a679a8766b5f0408d233ed40
karan8102 <121483288+karan8102@users.noreply.github.com>
2023-10-23 23:59:27 +0530

    demo.php

--- demo.php
+++ demo.php
@@ -1,48 +1,10 @@
 <?php
-// Establish a database connection using mysqli
-$host = 'localhost';
-$username = 'root';
-$password = '';  // Replace with your actual database password
-$database = 'project';
-
-$conn = new mysqli($host, $username, $password, $database);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Use prepared statements to insert data into the database
-$name = $_GET['name'];
-$phone = $_GET['phone'];
-
-// Prepare the SQL statement
-if ($stmt = $conn->prepare("INSERT INTO `res` (`name`, `number`) VALUES (?, ?)")) {
-    // Bind the parameters and execute the query
-    $stmt->bind_param("ss", $name, $phone);
-
-    if ($stmt->execute()) {
-        echo "Data inserted successfully.";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
-
-    // Close the statement
-    $stmt->close();
-}
-
-// Select and display data from the database
-$query = "SELECT * FROM `res`";
-$result = $conn->query($query);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"] . ", Phone: " . $row["number"] . "<br>";
-    }
-} else {
-    echo "No records found.";
-}
-
-// Close the database connection
-$conn->close();
-?>
+$cname=$_GET['cname'];
+$cphone=$_GET['cphone'];
+$c=mysqli_connect('localhost','root','','disha');
+$q="INSERT INTO `res`(`name`, `number`) VALUES
+('$cname','$cphone')";
+$a=mysqli_query($c,$q);
+$b="SELECT * FROM `res`";
+$d=mysqli_query($c,$b);
+?> 
