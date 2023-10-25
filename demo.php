281dade9c51813ccc8b54c017398c2a6a2c0f8cb
karan8102 <121483288+karan8102@users.noreply.github.com>
2023-10-25 21:03:28 +0530

    demo.php

--- demo.php
+++ demo.php
@@ -1,10 +1,29 @@
 <?php
-$cname=$_GET['cname'];
-$cphone=$_GET['cphone'];
-$c=mysqli_connect('localhost','root','','disha');
-$q="INSERT INTO `res`(`name`, `number`) VALUES
-('$cname','$cphone')";
-$a=mysqli_query($c,$q);
-$b="SELECT * FROM `res`";
-$d=mysqli_query($c,$b);
-?> 
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bookings";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the user input from the form
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+// Insert the booking data into the database
+$sql = "INSERT INTO booking (name, phone) VALUES ('$name', '$phone')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Booking created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
